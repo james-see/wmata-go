309624e165d5c8d2617d7f151d5d8a765cc32071
James Campbell <616585+james-see@users.noreply.github.com>
2023-01-17 17:02:18 -0500

    working client using localhost echo endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -12,14 +12,20 @@ import (
 
 func main() {
 	// load the env from app.env
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/echo", echoHandler)
+	http.ListenAndServe("localhost:5000", mux)
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
 	config, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
-
 	url := "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/C05"
 
-	payload := strings.NewReader("{body}")
+	payload := strings.NewReader("")
 
 	req, _ := http.NewRequest("GET", url, payload)
 
@@ -28,9 +34,43 @@ func main() {
 	res, _ := http.DefaultClient.Do(req)
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
+	// body, _ := ioutil.ReadAll(res.Body)
+	//
+	trains := util.Train{}
+	err = json.NewDecoder(res.Body).Decode(&trains)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Println(res)
-	fmt.Println(string(body))
+	// car := util.Car{}
+	// if err := json.Unmarshal(trains, &car); err != nil {
+	// 	panic(err)
+	// }
+	destination := &util.CurrentStatus{}
+
+	for _, car := range trains.Cars {
+		if car.DestinationCode == "K08" {
+			destination.Status = car.Min
+			destination.Destination = car.Destination
+			destination.LocationName = car.LocationName
+			break
+		}
+
+	}
+	fmt.Printf("Destination: %v\n", destination.Destination)
+	fmt.Printf("Time to Board: %v\n", destination.Status)
+
+	// fmt.Println(trains.Cars[0])
+
+	// for index, train := range trains {
+	// 	fmt.Println(train.CarID, index)
+	// }
+	// var p fastjson.Parser
+	// v, _ := p.Parse(body)
+	// fmt.Printf("foo=%v\n", v.GetStringBytes("0"))
+	// fmt.Printf("foo.0=%v\n", fastjson.GetBytes(body))
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
 
 }
--- util/util.go
+++ util/util.go
@@ -6,6 +6,27 @@ type Config struct {
 	WmataAPI string `mapstructure:"API_KEY"`
 }
 
+type Train struct {
+	Cars []Car `json:"Trains"`
+}
+type Car struct {
+	CarID           string `json:"Car"`
+	Destination     string `json:"Destination"`
+	DestinationCode string `json:"DestinationCode"`
+	DestinationName string `json:"DestinationName"`
+	Group           string `json:"Group"`
+	Line            string `json:"Line"`
+	LocationCode    string `json:"LocationCode"`
+	LocationName    string `json:"LocationName"`
+	Min             string `json:"Min"`
+}
+
+type CurrentStatus struct {
+	Status       string
+	LocationName string
+	Destination  string
+}
+
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
