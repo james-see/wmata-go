61cbd1fbd2983aff10134d600b10ea56a9bc82b3
James Campbell <james@jamescampbell.us>
2023-01-22 00:11:33 -0500

    updated

--- docs/app.c328ef1a.js
+++ docs/app.c328ef1a.js
@@ -6251,7 +6251,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59665" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55235" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
